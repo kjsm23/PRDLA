32071b37044f9a8e1d18f77a19c6fa912d6ba1bf
Georgy J. Ramos <diator@live.com>
2017-02-16 14:17:45 -0400

    Panorama commit db insert and geoloc fixed

--- src/app/shared/photo-helpers/photo-preview.component.ts
+++ src/app/shared/photo-helpers/photo-preview.component.ts
@@ -1,16 +1,32 @@
 /**
  * Created by --- on 1/21/2017.
  */
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit, AfterViewInit} from '@angular/core';
 
 import { Photo } from '../models';
+
+import 'pannellum';
+declare const pannellum : any;
+
 
 @Component({
   selector: 'photo-preview',
   templateUrl: './photo-preview.component.html'
 })
-export class PhotoPreviewComponent {
+export class PhotoPreviewComponent implements AfterViewInit{
   @Input() photo: Photo;
+
+  ngAfterViewInit(){
+    pannellum.viewer('panorama',
+      {
+        "type":"equirectangular",
+        "panorama": this.photo.pathPano,
+        "autoload": true
+
+      });
+  };
+
+
 
   onToggleFavorite(favorited: boolean) {
     this.photo['favorited'] = favorited;
